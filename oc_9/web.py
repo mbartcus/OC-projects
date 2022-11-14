5cd759b170e15dbad286787981c126908718e198
marius <marius.bartcus@gmail.com>
2022-11-14 16:12:53 +0100

    update web app for working with the azure functions

--- oc_9/web.py
+++ oc_9/web.py
@@ -28,7 +28,7 @@ if submit_button:
     with st.spinner('Wait for it...'):
         st.info(f'Your user is :  {user_id}')
 
-        article_score = requests.get("http://127.0.0.1:5000/api", params={"user_id": user_id, "recommand": recommandation_type}).json()
+        article_score = requests.get(" http://localhost:7071/api/HttpTriggerRecom", params={"user_id": user_id, "recommand": recommandation_type}).json()
         st.text(type(article_score))
         st.json(article_score)
         #for i, article in enumerate(article_score):
